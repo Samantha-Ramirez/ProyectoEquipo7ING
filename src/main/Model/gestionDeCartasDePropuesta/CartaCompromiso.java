9edba27d695cb5e92e6b936356268fd5f1011f17
Samantha-Ramirez <samantha.p.ramirez.y@gmail.com>
2024-07-28 14:07:31 -0400

    Revert "Cambios jesus"
    This reverts commit fd056a342c17cc56bbe0fc4329f80c89dafdc839.

--- src/main/Model/gestionDeCartasDePropuesta/CartaCompromiso.java
+++ src/main/Model/gestionDeCartasDePropuesta/CartaCompromiso.java
@@ -1,23 +1,5 @@
 package main.Model.gestionDeCartasDePropuesta;
 
 public class CartaCompromiso {
-    private String acuerdoRetribucion;
-    private String formularioGeneracion;
-
-    // Getters y Setters
-    public String getAcuerdoRetribucion() {
-        return acuerdoRetribucion;
-    }
-
-    public void setAcuerdoRetribucion(String acuerdoRetribucion) {
-        this.acuerdoRetribucion = acuerdoRetribucion;
-    }
-
-    public String getFormularioGeneracion() {
-        return formularioGeneracion;
-    }
-
-    public void setFormularioGeneracion(String formularioGeneracion) {
-        this.formularioGeneracion = formularioGeneracion;
-    }
+    
 }
--- src/main/Model/gestionDeCartasDePropuesta/CartaIntencion.java
+++ src/main/Model/gestionDeCartasDePropuesta/CartaIntencion.java
@@ -1,41 +1,5 @@
 package main.Model.gestionDeCartasDePropuesta;
 
 public class CartaIntencion {
-    private String informacion;
-    private String firma;
-    private String naturalezaDelCurso;
-    private String validez;
-
-    // Getters y Setters
-    public String getInformacion() {
-        return informacion;
-    }
-
-    public void setInformacion(String informacion) {
-        this.informacion = informacion;
-    }
-
-    public String getFirma() {
-        return firma;
-    }
-
-    public void setFirma(String firma) {
-        this.firma = firma;
-    }
-
-    public String getNaturalezaDelCurso() {
-        return naturalezaDelCurso;
-    }
-
-    public void setNaturalezaDelCurso(String naturalezaDelCurso) {
-        this.naturalezaDelCurso = naturalezaDelCurso;
-    }
-
-    public String getValidez() {
-        return validez;
-    }
-
-    public void setValidez(String validez) {
-        this.validez = validez;
-    }
+    
 }
