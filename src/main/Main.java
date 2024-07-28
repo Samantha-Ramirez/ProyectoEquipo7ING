dd7f5c05a0f695a53d5c4fb7a001d6e417db1a58
Samantha-Ramirez <samantha.p.ramirez.y@gmail.com>
2024-07-28 08:55:26 -0400

    Cambio de nombre de paquete gestion de propuesta 2807

--- src/main/Main.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public class Main {
-    public static void main(String[] args) {
-        // se ejecuta primero este metodo
-        // static porque no tiene que crear un objeto para ser usado
-    }
-}
