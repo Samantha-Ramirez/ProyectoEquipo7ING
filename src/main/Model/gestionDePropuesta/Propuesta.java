279d003c2d0dfe941ac5980892386c1077b7f266
Samantha-Ramirez <samantha.p.ramirez.y@gmail.com>
2024-07-28 17:15:17 -0400

    Cambio 2807

--- src/main/Model/gestionDePropuesta/Propuesta.java
+++ src/main/Model/gestionDePropuesta/Propuesta.java
@@ -35,41 +35,11 @@ public class Propuesta {
         this.fundamentacion = fundamentacion;
     }
 
-    public boolean registrarDatos() {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
-        String currentTime = LocalDateTime.now().format(dtf);
-        String basePath = "src/main/Data/";// direccion donde se guardara el txt(data)
-
-        String registroHoraFileName = basePath + nombreUsuario + "_registroHora.txt";//concatenacion del nombre del archivo de registro de Inicio y fin de sesion
-        String usuarioFileName = basePath + nombreUsuario + ".txt";//
-        archivoRegistroDeHora = registroHoraFileName;// asignando nombre de archivo donde estara el registro de entrada, para usar cuando se vaya a cerrar sesion
-        if (!Files.exists(Paths.get(usuarioFileName))) {// verifica si el archivo de usuario existe, en caso de que exista retorna falso, pues ese usuario ya esta registrado
-            try (FileWriter registroHoraWriter = new FileWriter(registroHoraFileName);
-                 FileWriter usuarioWriter = new FileWriter(usuarioFileName)) { // abre el arcihivo en modo lectura
-
-                // Escribe la hora actual en el archivo de registro de hora
-                registroHoraWriter.write("Fecha de Inicio: " + currentTime + "\n");
-
-                // Escribe los datos del usuario en el archivo del usuario
-                usuarioWriter.write( tipoUsuario + "\n");
-                usuarioWriter.write(clave + "\n");
-                usuarioWriter.write( registroHoraFileName + "\n");
-
-            } catch (IOException e) {
-                e.printStackTrace(); // manejo de excepciones con respecto a las entradas
-            }
-            return true;
-        }else{
-            // MOSTRAR MENSAJE DE ERROR :
-            return false;
-        }
-
+    public Boolean esCreacionValida(){
+        //guardar en txt
+        return true;
     }
 
-    public Propuesta crearPropuesta(int userId, int estado){
-        Propuesta propuesta = new Propuesta();
-        return propuesta;
-    }
     public void setEstado(int estado){
 
     }
